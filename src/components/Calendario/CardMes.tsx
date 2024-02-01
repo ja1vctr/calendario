7f172aaab17cc70e1cd6f8e920359db49522b08f
ja1vctr <jaovictor3690@gmail.com>
2024-02-01 16:43:04 -0300

    Adicionando mapeadores de inicio e final das semanas do mes

--- src/components/Calendario/CardMes.tsx
+++ src/components/Calendario/CardMes.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import moment from 'moment'
 
 export default function Mes({ month, year }: { month: string; year: number }) {
   const [value, setValue] = useState(
     moment().locale('pt').month(month).year(year)
   )
+  const [calendario, setCalendario] = useState([])
+
+  useEffect(() => {
+    const startDay = value.clone().startOf('month').startOf('week')
+    const endDay = value.clone().endOf('month').endOf('week')
+  }, [value])
 
   return (
     <div>
